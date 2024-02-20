869fb1a9a7711fc44885b407f286f57c97bbcab1
takubii <vicsat93@gmail.com>
2024-02-20 16:48:33 +0900

    add: 初期画面作成

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,44 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import { Form, Links, LiveReload, Meta, Scripts, ScrollRestoration } from '@remix-run/react';
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        <div id='sidebar'>
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id='search-form' role='search'>
+              <input
+                id='q'
+                aria-label='Search contacts'
+                placeholder='Search'
+                type='search'
+                name='q'
+              />
+              <div id='search-spinner' aria-hidden hidden={true} />
+            </Form>
+            <Form method='post'>
+              <button type='submit'>New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href={`/contacts/1`}>Your Name</a>
+              </li>
+              <li>
+                <a href={`/contacts/2`}>Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ["**/.*"],
+module.exports = {
+  ignoredRouteFiles: ['**/.*'],
+  serverModuleFormat: 'cjs',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
