276961737c1c54a273c41da53552f435b8c39993
takubii <vicsat93@gmail.com>
2024-03-01 14:10:03 +0900

    add: 読み込み時の処理を追加

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from '@remix-run/react';
 import appStylesHref from './app.css';
 import { createEmptyContact, getContacts } from './data';
@@ -27,6 +28,7 @@ export const action = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang='en'>
@@ -84,7 +86,7 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id='detail'>
+        <div className={navigation.state === 'loading' ? 'loading' : ''} id='detail'>
           <Outlet />
         </div>
 
